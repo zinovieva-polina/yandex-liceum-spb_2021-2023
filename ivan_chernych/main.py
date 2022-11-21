df682630ecec5fada46163c57f05ce09d0208f77
alexandra <alexamedved06@gmail.com>
2022-11-21 20:05:12 +0300

    new

--- ivan_chernych/main.py
+++ ivan_chernych/main.py
@@ -1,9 +1,25 @@
 import pygame
 
+
+def draw(screen, w, h):
+    screen.fill((0, 0, 0))
+    pygame.draw.line(screen, (255, 255, 255),
+                     [0, 0],
+                     [w, h], 5)
+    pygame.draw.line(screen, (255, 255, 255),
+                     [w, 0],
+                     [0, h], 5)
+
+
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 800, 600
+    sz = input().split()
+    width, height = int(sz[0]), int(sz[1])
+    size = width, height
     screen = pygame.display.set_mode(size)
 
+
 while pygame.event.wait().type != pygame.QUIT:
-    pass
+    draw(screen, width, height)
+    pygame.display.flip()
+pygame.quit()
