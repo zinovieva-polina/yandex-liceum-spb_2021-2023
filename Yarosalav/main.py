f7d3c06d549c7a19b54bca9029ae64ca908c24ee
Samokhin Yaroslavv <samokhin.yaroslav.fake@gmail.com>
2022-11-21 19:31:45 +0300

    Добавленны шрифты

--- Yarosalav/main.py
+++ Yarosalav/main.py
@@ -3,16 +3,16 @@ import pygame
 
 def draw(screen):
     # "Fonts/Roboto_Bold.ttf"
-    screen.fill((255, 0, 0))
-    font = pygame.font.Font(None, 60)
-    # text = font.render("Я сделал это!", True, (34, 255, 0))
-    # text_x = width // 2 - text.get_width() // 2
-    # text_y = height // 2 - text.get_height() // 2
-    # text_w = text.get_width()
-    # text_h = text.get_height()
-    # screen.blit(text, (text_x, text_y))
-    # pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
-    #                                        text_w + 20, text_h + 20), 1)
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font("Fonts/Roboto-Bold.ttf", 60)
+    text = font.render("Я сделал это!", True, (34, 255, 0))
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
+                                           text_w + 20, text_h + 20), 1)
 
 
 if __name__ == '__main__':
