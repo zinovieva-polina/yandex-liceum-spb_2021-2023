95d8b0f74a35f7bb23f7351e71105b6da3a5f7b0
Samokhin Yaroslavv <samokhin.yaroslav.fake@gmail.com>
2022-11-21 19:37:26 +0300

    Кресты

--- Yarosalav/main.py
+++ Yarosalav/main.py
@@ -1,31 +1,48 @@
+# import pygame
+#
+#
+# def draw(screen):
+#     # "Fonts/Roboto_Bold.ttf"
+#     screen.fill((0, 0, 0))
+#     font = pygame.font.Font("Fonts/Roboto-Bold.ttf", 60)
+#     text = font.render("Я сделал это!", True, (34, 255, 0))
+#     text_x = width // 2 - text.get_width() // 2
+#     text_y = height // 2 - text.get_height() // 2
+#     text_w = text.get_width()
+#     text_h = text.get_height()
+#     screen.blit(text, (text_x, text_y))
+#     pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
+#                                            text_w + 20, text_h + 20), 1)
+#
+#
+# if __name__ == '__main__':
+#     # инициализация Pygame:
+#     pygame.init()
+#     # размеры окна:
+#     size = width, height = 800, 600
+#     # screen — холст, на котором нужно рисовать:
+#     screen = pygame.display.set_mode(size)
+#     # ожидание закрытия окна:
+#     while pygame.event.wait().type != pygame.QUIT:
+#         draw(screen)
+#         # смена (отрисовка) кадра:
+#         pygame.display.flip()
+#     # завершение работы:
+#     pygame.quit()
+
 import pygame
 
-
 def draw(screen):
-    # "Fonts/Roboto_Bold.ttf"
     screen.fill((0, 0, 0))
-    font = pygame.font.Font("Fonts/Roboto-Bold.ttf", 60)
-    text = font.render("Я сделал это!", True, (34, 255, 0))
-    text_x = width // 2 - text.get_width() // 2
-    text_y = height // 2 - text.get_height() // 2
-    text_w = text.get_width()
-    text_h = text.get_height()
-    screen.blit(text, (text_x, text_y))
-    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
-                                           text_w + 20, text_h + 20), 1)
+    pygame.draw.line(screen, (255, 255, 255), (0, 0), (800, 600), width=5)
+    pygame.draw.line(screen, (255, 255, 255), (800, 0), (0, 600), width=5)
 
 
 if __name__ == '__main__':
-    # инициализация Pygame:
     pygame.init()
-    # размеры окна:
     size = width, height = 800, 600
-    # screen — холст, на котором нужно рисовать:
     screen = pygame.display.set_mode(size)
-    # ожидание закрытия окна:
     while pygame.event.wait().type != pygame.QUIT:
         draw(screen)
-        # смена (отрисовка) кадра:
         pygame.display.flip()
-    # завершение работы:
     pygame.quit()
