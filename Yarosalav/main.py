8b98140b65413c51de58bbb038fc654f8072ab6a
Samokhin Yaroslavv <samokhin.yaroslav.fake@gmail.com>
2022-11-21 18:57:54 +0300

    Создал окно

---
+++ Yarosalav/main.py
@@ -0,0 +1,20 @@
+import pygame
+
+if __name__ == '__main__':
+    # инициализация Pygame:
+    pygame.init()
+    # размеры окна:
+    size = width, height = 800, 600
+    # screen — холст, на котором нужно рисовать:
+    screen = pygame.display.set_mode(size)
+    # формирование кадра:
+    # команды рисования на холсте
+    # ...
+    # ...
+    # смена (отрисовка) кадра:
+    pygame.display.flip()
+    # ожидание закрытия окна:
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    # завершение работы:
+    pygame.quit()
