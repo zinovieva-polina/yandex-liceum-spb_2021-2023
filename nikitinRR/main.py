1ce512b17f7290622ab9cf1017f41c1cf8c1d183
Nikitin Rat <rote808out@gmail.com>
2022-11-21 19:49:46 +0300

    сделал папку и файл and noname code

---
+++ nikitinRR/main.py
@@ -0,0 +1,35 @@
+import pygame
+
+
+def draw(screen):
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 50)
+    text = font.render("Hello, Pygame!", True, (100, 255, 100))
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+    pygame.draw.rect(screen, (0, 255, 0), (text_x - 10, text_y - 10,
+                                           text_w + 20, text_h + 20), 1)
+
+
+if __name__ == '__main__':
+    # инициализация Pygame:
+    pygame.init()
+    # размеры окна:
+    size = width, height = 800, 600
+    # screen — холст, на котором нужно рисовать:
+    screen = pygame.display.set_mode(size)
+    draw(screen)
+    # формирование кадра:
+    # команды рисования на холсте
+    # ...
+    # ...
+    # смена (отрисовка) кадра:
+    pygame.display.flip()
+    # ожидание закрытия окна:
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    # завершение работы:
+    pygame.quit()
