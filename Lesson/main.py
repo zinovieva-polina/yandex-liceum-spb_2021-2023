0fedb4e29aed0418954a70c80716535dc65681ce
P.Zinoveva <zinovevapa@yandexlyceum.ru>
2022-11-24 18:51:19 +0300

    lesson update

--- Lesson/main.py
+++ Lesson/main.py
@@ -4,7 +4,9 @@ if __name__ == '__main__':
     pygame.init()
     size = width, height = 800, 400
     screen = pygame.display.set_mode(size)
-
+    x_pos = -10
+    v = 60
+    clock = pygame.time.Clock()
     running = True
     while running:
         # внутри игрового цикла ещё один цикл
@@ -13,7 +15,9 @@ if __name__ == '__main__':
             # при закрытии окна
             if event.type == pygame.QUIT:
                 running = False
-
+        screen.fill((0, 0, 0))
+        pygame.draw.circle(screen, (255, 0, 0), (x_pos, 200), 20)
+        x_pos += v * clock.tick() / 1000  # v * t в секундах
         # отрисовка и изменение свойств объектов
         # ...
 
