02b9418180fafc3211e98a32dd3bfb141544b0d0
EastCoast <winterofficial000@gmail.com>
2022-12-01 19:58:25 +0300

    s

--- sasha/main.py
+++ sasha/main.py
@@ -1,8 +1,13 @@
 import pygame
-from board import Board
+from board.boardlife import BoardGameLife
 
 
 def game(screen, board):
+    start = False
+    FPS = 60
+    tick = 0
+    speed = 1
+    clock = pygame.time.Clock()
     running = True
     while running:
         for event in pygame.event.get():
@@ -10,8 +15,25 @@ def game(screen, board):
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 board.get_click(event.pos)
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                start = True
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                if start:
+                    start = False
+                else:
+                    start = True
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+                speed += 1
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+                speed -= 1
+
         screen.fill((0, 0, 0))
+        if start:
+            if tick >= speed:
+                board.next_move()
         board.render(screen)
+        tick += 1
+        clock.tick(FPS)
         pygame.display.flip()
 
 
@@ -22,5 +44,5 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     pygame.display.set_caption('Игра «Жизнь»')
 
-    board = Board(30, 30, 10, 10, 15)
+    board = BoardGameLife(30, 30, 10, 10, 15)
     game(screen, board)
